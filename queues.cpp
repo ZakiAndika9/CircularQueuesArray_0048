a90947194e92db081f9caadb4bb9685d56d7d5a0
ZakiAndika9 <m.zaki.andika9@gmail.com>
2025-06-12 11:03:40 +0700

    Add insert logic with user input to queue

--- queues.cpp
+++ queues.cpp
@@ -12,3 +12,9 @@ public:
         front = -1;
         rear = -1;
     }
+    void insert()
+    {
+        int num;
+        cout << "Enter a number : ";
+        cin >> num;
+        cout << endl;
